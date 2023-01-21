5bb15f14a067f001c5618309ad00103c13384d83
Pielgrzy <47796557+Pielgrzy@users.noreply.github.com>
2023-01-21 12:24:59 +0100

    Add files via upload

---
+++ Java8+/src/L_5_Aggregation/E_1/BankAccount.java
@@ -0,0 +1,6 @@
+package L_5_Aggregation.E_1;
+
+public class BankAccount {
+     Person owner;
+     double balance;
+}
---
+++ Java8+/src/L_5_Aggregation/E_1/Credit.java
@@ -0,0 +1,8 @@
+package L_5_Aggregation.E_1;
+
+public class Credit {
+     Person borrower;
+     double credit;
+     double leftCredit;
+     double percentage;
+}
